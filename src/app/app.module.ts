ab783af0ebbaa78ce521786b532bea6f736562d1
Christopher Castrillon <christophercastrillon@Nicholass-MacBook-Pro.local>
2021-02-01 16:04:39 -0500

    added bootstrap, navbar and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CreatAccountComponent } from './creat-account/creat-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    CreatAccountComponent
   ],
   imports: [
     BrowserModule,
