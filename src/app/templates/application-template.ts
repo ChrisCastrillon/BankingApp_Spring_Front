37888e4381bec9fe5c503d49c9383963a62dcf73
Christopher Castrillon <christophercastrillon@Nicholass-MBP.home>
2021-02-09 17:48:06 -0500

    refactored login logic, created login to apply for accounts, register for site acccounts, as well as application service, and templates

---
+++ src/app/templates/application-template.ts
@@ -0,0 +1,9 @@
+export class ApplicationTemplate {
+    public accountType: string;
+    public salary: number;
+    public jobTitle: string;
+    public firstname: string;
+    public lastname: string;
+    public ssn: number;
+    public email: string;
+}
---
+++ src/app/templates/registration-form-template.ts
@@ -0,0 +1,9 @@
+import { ɵangular_packages_platform_browser_dynamic_platform_browser_dynamic_a } from '@angular/platform-browser-dynamic';
+
+export class RegistrationFormTemplate {
+    public firstname: string;
+    public lastname: string;
+    public email: string;
+    public username: string;
+    public password: string;
+}
